7415a12fea4f7d05ce85253d4eb85a5985fb3e5b
koffein4ik <koffein4ik@gmail.com>
2020-04-13 21:48:55 +0300

    Url columns for pictures were added to albums, artists, playlists. Url for song was added.

--- music_store.sql
+++ music_store.sql
@@ -1,85 +1,126 @@
-CREATE TABLE `users` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255),
-  `surname` varchar(255),
-  `nickname` varchar(255),
-  `password` varchar(255),
-  `user_role_id` int
+create table artist
+(
+	artist_id int auto_increment
+		primary key,
+	name varchar(255) null,
+	picture_url varchar(255) null
 );
 
-CREATE TABLE `songs` (
-  `song_id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255),
-  `genre_id` int,
-  `album_id` int,
-  `is_cover` boolean
+create table album
+(
+	album_id int auto_increment
+		primary key,
+	name varchar(255) null,
+	release_date datetime null,
+	artist_id int null,
+	picture_url varchar(255) null,
+	constraint album_ibfk_1
+		foreign key (artist_id) references artist (artist_id)
 );
 
-CREATE TABLE `album` (
-  `album_id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255),
-  `release_date` datetime,
-  `artist_id` int
+create index artist_id
+	on album (artist_id);
+
+create table genres
+(
+	genre_id int auto_increment
+		primary key,
+	name int null
 );
 
-CREATE TABLE `artist` (
-  `artist_id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255)
+create table songs
+(
+	song_id int auto_increment
+		primary key,
+	name varchar(255) null,
+	genre_id int null,
+	album_id int null,
+	is_cover tinyint(1) null,
+	url varchar(255) null,
+	constraint songs_ibfk_1
+		foreign key (genre_id) references genres (genre_id),
+	constraint songs_ibfk_2
+		foreign key (album_id) references album (album_id)
 );
 
-CREATE TABLE `genres` (
-  `genre_id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` int
+create table song_artist
+(
+	song_id int not null,
+	artist_id int not null,
+	primary key (song_id, artist_id),
+	constraint song_artist_ibfk_1
+		foreign key (artist_id) references artist (artist_id),
+	constraint song_artist_ibfk_2
+		foreign key (song_id) references songs (song_id)
 );
 
-CREATE TABLE `playlist` (
-  `playlist_id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255),
-  `user_id` int
+create index artist_id
+	on song_artist (artist_id);
+
+create index album_id
+	on songs (album_id);
+
+create index genre_id
+	on songs (genre_id);
+
+create table user_role
+(
+	user_role_id int auto_increment
+		primary key,
+	name varchar(255) null
 );
 
-CREATE TABLE `playlist_song` (
-  `playlist_id` int,
-  `song_id` int,
-  PRIMARY KEY (`playlist_id`, `song_id`)
+create table users
+(
+	id int auto_increment
+		primary key,
+	name varchar(255) null,
+	surname varchar(255) null,
+	nickname varchar(255) null,
+	password varchar(255) null,
+	user_role_id int null,
+	constraint users_ibfk_1
+		foreign key (user_role_id) references user_role (user_role_id)
 );
 
-CREATE TABLE `user_role` (
-  `user_role_id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255)
+create table playlist
+(
+	playlist_id int auto_increment
+		primary key,
+	name varchar(255) null,
+	user_id int null,
+	picture_url varchar(255) null,
+	constraint playlist_ibfk_1
+		foreign key (user_id) references users (id)
 );
 
-CREATE TABLE `user_preferences` (
-  `user_id` int,
-  `genre_id` int,
-  PRIMARY KEY (`user_id`, `genre_id`)
+create index user_id
+	on playlist (user_id);
+
+create table playlist_song
+(
+	playlist_id int not null,
+	song_id int not null,
+	primary key (playlist_id, song_id),
+	constraint playlist_song_ibfk_1
+		foreign key (playlist_id) references playlist (playlist_id),
+	constraint playlist_song_ibfk_2
+		foreign key (playlist_id) references songs (song_id)
 );
 
-CREATE TABLE `song_artist` (
-  `song_id` int,
-  `artist_id` int,
-  PRIMARY KEY (`song_id`, `artist_id`)
+create table user_preferences
+(
+	user_id int not null,
+	genre_id int not null,
+	primary key (user_id, genre_id),
+	constraint user_preferences_ibfk_1
+		foreign key (user_id) references users (id),
+	constraint user_preferences_ibfk_2
+		foreign key (genre_id) references genres (genre_id)
 );
 
-ALTER TABLE `users` ADD FOREIGN KEY (`user_role_id`) REFERENCES `user_role` (`user_role_id`);
+create index genre_id
+	on user_preferences (genre_id);
 
-ALTER TABLE `user_preferences` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `user_preferences` ADD FOREIGN KEY (`genre_id`) REFERENCES `genres` (`genre_id`);
-
-ALTER TABLE `songs` ADD FOREIGN KEY (`genre_id`) REFERENCES `genres` (`genre_id`);
-
-ALTER TABLE `songs` ADD FOREIGN KEY (`album_id`) REFERENCES `album` (`album_id`);
-
-ALTER TABLE `song_artist` ADD FOREIGN KEY (`artist_id`) REFERENCES `artist` (`artist_id`);
-
-ALTER TABLE `song_artist` ADD FOREIGN KEY (`song_id`) REFERENCES `songs` (`song_id`);
-
-ALTER TABLE `playlist` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `playlist_song` ADD FOREIGN KEY (`playlist_id`) REFERENCES `playlist` (`playlist_id`);
-
-ALTER TABLE `playlist_song` ADD FOREIGN KEY (`playlist_id`) REFERENCES `songs` (`song_id`);
-
-ALTER TABLE `album` ADD FOREIGN KEY (`artist_id`) REFERENCES `artist` (`artist_id`);
-
+create index user_role_id
+	on users (user_role_id);
