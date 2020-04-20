4466d1d733146af92ca1adb68f2219ebc20c7734
Polina Sergey <polina.sergey.d@gmail.com>
2020-04-20 15:06:53 +0300

    Edited search of songs, artists and albums function: added layers, reduced quantity of requests to database

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -1,41 +1,14 @@
-const mysql = require('mysql2');
-const config = require('../config/mysqlconfig');
-const sqlQueries = require('../models/SQL');
-const expirationTime = 3600;
-const connection = mysql.createConnection(config.config);
+const searchService = require('../services/searchService');
 
 exports.search = async function(request, response) {
-    const albums = (await findAlbumsInDB(request.body.searchText))[0];
-    const artists = (await findArtistsInDB(request.body.searchText))[0];
-    var songs = (await findSongsInDB(request.body.searchText))[0];
-    console.log(request.body.searchText);
-
-    for(var i = 0; i<songs.length; i++){
-        const artistsId= (await findArtistsOfSongInBD(songs[i].song_id))[0];
-        var artists_of_song = [];
-        for (var j = 0; j<artistsId.length; j++){
-            artists_of_song[j] = (await findArtistByIdInDB(artistsId[j].artist_id))[0][0];    
-        }
-        songs[i] = [songs[i], artists_of_song];
-    }
-        
+    const albums = (await searchService.findAlbumsInDB(request.body.searchText));
+    const artists = (await searchService.findArtistsInDB(request.body.searchText));
+    const songs = (await searchService.findSongsAndItsArtistsInDB(request.body.searchText));
+    
     response.status(200).json({albums,artists,songs});
 };
 
-async function findAlbumsInDB(searchRow) {
-    return await connection.promise().query(sqlQueries.FIND_ALBOMS_BY_NAME, [`%${searchRow}%`]);
-};
-async function findArtistsInDB(searchRow) {
-    return await connection.promise().query(sqlQueries.FIND_ARTISTS_BY_NAME, [`%${searchRow}%`]);
-};
-async function findSongsInDB(searchRow) {
-    return await connection.promise().query(sqlQueries.FIND_SONGS_BY_NAME, [`%${searchRow}%`]);
-};
 
-async function findArtistsOfSongInBD(searchRow){
-    return await connection.promise().query(sqlQueries.FIND_ARTISTS_OF_SONG, [searchRow]);
-}
 
-async function findArtistByIdInDB(searchRow){
-    return await connection.promise().query(sqlQueries.FIND_ARTIST_BY_ID, [searchRow])
-}
+
+
--- backend/dao/databaseService.js
+++ backend/dao/databaseService.js
@@ -10,5 +10,18 @@ const executeQuery = async function (query, params) {
 exports.executeQuery = executeQuery;
 
 exports.getPlaylistsByUserId = async function (user_id) {
-     return await executeQuery(sqlQueries.FIND_PLAYLISTS_BY_USER_ID, user_id)
+    return await executeQuery(sqlQueries.FIND_PLAYLISTS_BY_USER_ID, user_id);
 };
+
+exports.findAlbumsInDB = async function (searchRow) {
+    return await executeQuery(sqlQueries.FIND_ALBOMS_BY_NAME, [`%${searchRow}%`]);
+};
+
+exports.findArtistsInDB = async function (searchRow){
+    return await executeQuery(sqlQueries.FIND_ARTISTS_BY_NAME, [`%${searchRow}%`]);
+};
+
+exports.findSongsAndItsArtistsInDB = async function (searchRow){
+    return await executeQuery(sqlQueries.FIND_SONGS_BY_NAME_WITH_ITS_ARTISTS, [`%${searchRow}%`]);
+};
+
