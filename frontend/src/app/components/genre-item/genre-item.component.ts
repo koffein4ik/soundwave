d1a5e01e2a25dbaec73a03a8c068393a095d1a34
Ilyaleshka <oleshko.ivl@gmail.com>
2020-04-26 17:47:42 +0300

    Added genre picker component

--- frontend/src/app/components/genre-item/genre-item.component.ts
+++ frontend/src/app/components/genre-item/genre-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Genre } from 'src/app/models/genre.model';
 
 @Component({
   selector: 'app-genre-item',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenreItemComponent implements OnInit {
 
+  @Input()
+  genre: Genre;
+
+  @Input()
+  color: string;
+
+  @Output()
+  onClick = new EventEmitter<any>();;
+
+  hover: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  get hoverColor(): string{
+    return this.hover ? this.color : "transparent"
+  }
+
+  get textColor(): string{
+    return this.hover ? this.color : "white"
+  }
+
+  get imgSrc(): string{
+    return "assets/" + this.genre.name.replace(" ","").toLowerCase() + ".png";
   }
 
 }
--- frontend/src/app/pages/radio-page/radio-page.component.ts
+++ frontend/src/app/pages/radio-page/radio-page.component.ts
@@ -9,12 +9,23 @@ import {Genre} from "../../models/genre.model";
 })
 export class RadioPageComponent implements OnInit {
 
+  private genres : Genre[];
+  colors: string[] = ["ff6665","3779bc","6c65a9","59cd9c","a5c94d","ffbb5a","6fc3e0","3333ff","e43c31","c44f69","9d65a9"]
+
   constructor(private genreService: GenreService) { }
 
   ngOnInit() {
     this.genreService.getGenres().subscribe((value: Genre[]) => {
-      console.log(value);
+      this.genres = value;
     })
+  }
+
+  getColor(index: number): string{
+    return '#' + this.colors[index % this.colors.length];
+  }
+
+  chooseGenre(index: number){
+      console.log(this.genres[index])
   }
 
 }
