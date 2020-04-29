98edcfb31b0a04563265a59549373d22be4f08d5
koffein4ik <koffein4ik@gmail.com>
2020-04-29 23:56:52 +0300

    Converter updated.

--- backend/converters/songConverter.js
+++ backend/converters/songConverter.js
@@ -1,13 +1,19 @@
-exports.convertSong = function(song) {
+exports.convertSong = function (song) {
     let songModel = {};
     songModel.name = song.songs_name;
     songModel.picture_url = song.album_picture_url;
     songModel.song_id = song.song_id;
     songModel.url = song.url;
     const artistModel = {
-        name : song.artist_name,
+        name: song.artist_name,
         id: song.artist_id
     };
     songModel.artist = artistModel;
+    const albumModel = {
+        name: song.album_name,
+        id: song.album_id,
+        pictureURL: song.album_picture_url
+    };
+    songModel.album = albumModel;
     return songModel;
 };
